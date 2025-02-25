e04481e18134822937f143e2b524028d6a89685e
Khanhlaptrinh <buiduykhanh0162003@gmail.com>
2025-02-25 16:47:50 -0700

    first commit

--- lession06_app/lib/main.dart
+++ lession06_app/lib/main.dart
@@ -9,9 +9,37 @@ import 'package:flutter/material.dart';
 void main() {
   //Bước 3: Khởi động appapp
   runApp(
-    const MaterialApp(
-      //Bướcước 4: Sử dụng thành phần để xây dựng giao diện gười dùng
-      home: Text('Hello World'),
+    MaterialApp(
+      //Bước 4: Sử dụng thành phần để xây dựng giao diện gười dùng
+      home: SafeArea(
+        child: Scaffold(
+          body: Center(child: MyWidget(false)),
+          // appBar: AppBar(
+          //   backgroundColor: Colors.red,
+          //   title: Text('Tu hoc Flutter'),
+          // ),
+          // body: const Center(child: Text('Hello World')),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
     ),
   );
+}
+
+class MyWidget extends StatelessWidget {
+  final bool loading;
+
+  MyWidget(this.loading);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    // if (loading == true) {
+    //   return const CircularProgressIndicator();
+    // } else {
+    //   return const Text('State');
+    // }
+
+    return loading ? const CircularProgressIndicator() : const Text('State');
+  }
 }
