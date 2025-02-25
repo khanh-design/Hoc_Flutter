075f352cd1f767e35360a0d3ccba5f5e76ec6124
Khanhlaptrinh <buiduykhanh0162003@gmail.com>
2025-02-25 09:01:55 -0700

    first commit

---
+++ lession05_const_final/android/app/src/main/kotlin/com/example/lession05_const_final/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lession05_const_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lession05_const_final/lib/user.dart
@@ -0,0 +1,6 @@
+class User {
+  final int id; // khong thay doi trong khoang thoi gian
+  static const String name = 'MY_TAG';
+
+  User(this.id);
+}
