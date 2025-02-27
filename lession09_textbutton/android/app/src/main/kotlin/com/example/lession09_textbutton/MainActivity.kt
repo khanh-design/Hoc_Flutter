0a5a424ee430a4170b4ecc98d725477ee37e1ade
Khanhlaptrinh <buiduykhanh0162003@gmail.com>
2025-02-26 21:56:44 -0700

    first commit

---
+++ lession09_textbutton/android/app/src/main/kotlin/com/example/lession09_textbutton/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lession09_textbutton
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lession09_textbutton/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: SafeArea(
+      child: Scaffold(
+      body: MyWidget(),
+    )),
+  ));
+}
+
+class MyWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
