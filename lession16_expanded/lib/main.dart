3bf98c82fdd0b053cda72cb35ec597db7a439c60
Khanh-desgin <buiduykhanh1062003@gmail.com>
2025-03-04 04:52:18 -0700

    first commit

--- lession16_expanded/lib/main.dart
+++ lession16_expanded/lib/main.dart
@@ -9,11 +9,27 @@ void main() {
   );
 }
 
+/// - Expanded: Là Widget có thể lấp đầy khoảng trống của thăng Row, Column
+/// - Và nó có thể xét tỷ lệ.
+
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: Row(
+        children: [
+          Expanded(flex: 1, child: Container(color: Colors.green, height: 150)),
+          SizedBox(width: 10),
+          Expanded(flex: 2, child: Container(color: Colors.blue, height: 150)),
+          SizedBox(width: 10),
+          Expanded(
+            flex: 1,
+            child: Container(color: Colors.yellow, height: 150),
+          ),
+        ],
+      ),
+    );
   }
 }
